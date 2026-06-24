3224fbbca23c066c5769e8de729f6478d6c5b816
Nguyễn Đại Trường Danh <23520242@gm.uit.edu.vn>
2026-06-24 13:09:45 +0700

    use hf inference to run embedding model

--- apps/api/src/lib/ai/ai.module.ts
+++ apps/api/src/lib/ai/ai.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { OllamaAiProvider } from './ollama-ai.provider';
+import { HuggingFaceAiProvider } from './hugging-face-ai.provider';
 
 @Module({
   imports: [ConfigModule],
-  providers: [OllamaAiProvider],
-  exports: [OllamaAiProvider],
+  providers: [OllamaAiProvider, HuggingFaceAiProvider],
+  exports: [OllamaAiProvider, HuggingFaceAiProvider],
 })
 export class AiModule {}
